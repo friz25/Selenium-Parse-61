3a7b45c895ba3f9bd8e379844241778673071e6e
frizmob@gmail.com <friz48625>
2022-06-24 11:27:10 +0300

    Реализовал: Поиск (названий) первых 10ти видео

--- 1.py
+++ 1.py
@@ -7,8 +7,33 @@ import time
 # Указываем полный путь к geckodriver.exe на вашем ПК.
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome('C:\\PythonProjects\\Selenium61\\chromedriver.exe')
-driver.get("https://www.youtube.com/")
-time.sleep(4)
-element = driver.find_element(By.XPATH, """/html/body/ytd-app/div[1]/ytd-mini-guide-renderer/div/ytd-mini-guide-entry-renderer[3]/a""")
-element.click()
+class SileniumTest:
+    driver = webdriver.Chrome('C:\\PythonProjects\\Selenium61\\chromedriver.exe')
+    driver.get("https://www.youtube.com/")
+    # time.sleep(4)
+    # element = driver.find_element(By.XPATH, """/html/body/ytd-app/div[1]/ytd-mini-guide-renderer/div/ytd-mini-guide-entry-renderer[3]/a""")
+    # element.click()
+
+    """Поиск канала на YouTube"""
+    time.sleep(1)
+    search_bar = driver.find_element(By.XPATH, """//input[@id="search"]""")
+    search_bar.send_keys("Гоблин")
+    # print("search_bar.send_keys('Гоблин')")
+    search_button = driver.find_element(By.XPATH, """/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/button""")
+    search_button.click()
+    # print("search_button.click()")
+
+    """Поиск (названий) первых 10ти видео"""
+    time.sleep(1)
+    videos = driver.find_elements(By.ID, """title-wrapper""")
+    print("")
+    for i in range(len(videos)):
+        print(videos[i].text)
+        if i == 10:
+            break
+    """Выведем ссылку на первое видео"""
+    print("")
+    print(videos[0].get_attribute('href'))
+
+if __name__ == '__main__':
+    SileniumTest()
